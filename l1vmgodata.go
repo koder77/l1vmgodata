4861a3a82e65a33f8cf8dc1cbc0bbed6f1f4e6bf
Stefan Pietzonke <spietzonke@gmail.com>
2022-04-13 16:12:30 +0200

    comment header update

--- l1vmgodata.go
+++ l1vmgodata.go
@@ -6,18 +6,18 @@
  *
  * (c) Copyright Stefan Pietzonke (jay-t@gmx.net), 2022
  *
- * L1vm is free software: you can redistribute it and/or modify
+ * L1VMgodata is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * L1vm is distributed in the hope that it will be useful,
+ * L1VMgodata is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with L1vm.  If not, see <http://www.gnu.org/licenses/>.
+ * along with L1VMgodata.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // tutorial code for tcp/ip sockets taken from: https://www.developer.com/languages/intro-socket-programming-go/
